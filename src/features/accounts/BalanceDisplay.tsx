9b8f20d9c728bcb06e215c87a40d86c80f6700bc
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-09 19:56:47 +0400

    create formatCurrnecy

--- src/features/accounts/BalanceDisplay.tsx
+++ src/features/accounts/BalanceDisplay.tsx
@@ -1,13 +1,20 @@
 import { useSelector } from "react-redux";
 import { rootState } from "../../store";
 
+function formatCurrnecy(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
 export default function BalanceDisplay() {
   const balance = useSelector(
     (state: rootState) => state.account.balance
   );
   return (
     <div>
-      <h1>{balance}</h1>
+      <h1>{formatCurrnecy(balance)}</h1>
     </div>
   );
 }
